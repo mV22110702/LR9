4bd40480b474a2918003ca507f9b062dbfc6a928
mV22110702 <fieldlavender70@gmail.com>
2022-10-20 09:57:19 +0300

    Uploaded site

---
+++ script.js
@@ -0,0 +1,151 @@
+const scoreTable = document.getElementsByClassName("scoreBoard")[0];
+
+document.getElementById("userName1").innerText = prompt("Enter player 1 name:");
+document.getElementById("userName2").innerText = prompt("Enter player 2 name:");
+
+document.getElementsByTagName("button")[0].addEventListener("mouseover",function (event){
+  event.target.style.setProperty("transition-duration","5000ms");
+  event.target.style.setProperty("background-color","rgb(226,255,216)");
+  event.target.style.setProperty("background","radial-gradient(circle, rgba(255,255,255,0.9502071092108718) 5%, rgba(225,240,240,1) 22%, rgba(0,255,0,1) 100%)");
+});
+document.getElementsByTagName("button")[1].addEventListener("mouseover",function (event){
+  event.target.style.setProperty("transition-duration","5000ms");
+  event.target.style.setProperty("background-color","rgb(223,224,224)");
+  event.target.style.setProperty("background","radial-gradient(circle, rgba(255,255,255,0.9502071092108718) 5%, rgba(225,240,240,1) 22%, rgba(0,255,255,1) 100%)");
+});
+document.getElementsByTagName("button")[0].addEventListener("mouseout",function (event){
+  event.target.removeAttribute("style");
+});
+document.getElementsByTagName("button")[1].addEventListener("mouseout",function (event){
+  event.target.removeAttribute("style");
+});
+
+const userNumElement1 = document.getElementById("userNumber1");
+const userNumElement2 = document.getElementById("userNumber2");
+
+const userScoreElement1 = document.getElementById("userScore1");
+const userScoreElement2 = document.getElementById("userScore2");
+
+alert("Here");
+const cards=[
+  {"dir":`url('6_of_clubs.png')`,"weight":6},
+  {"dir":`url('6_of_diamonds.png')`,"weight":6},
+  {"dir":`url('6_of_hearts.png')`,"weight":6},
+  {"dir":`url('6_of_spades.png')`,"weight":6},
+
+  {"dir":`url('7_of_clubs.png')`,"weight":7},
+  {"dir":`url('7_of_diamonds.png')`,"weight":7},
+  {"dir":`url('7_of_hearts.png')`,"weight":7},
+  {"dir":`url('7_of_spades.png')`,"weight":7},
+
+  {"dir":`url('8_of_clubs.png')`,"weight":8},
+  {"dir":`url('8_of_diamonds.png')`,"weight":8},
+  {"dir":`url('8_of_hearts.png')`,"weight":8},
+  {"dir":`url('8_of_spades.png')`,"weight":8},
+
+  {"dir":`url('9_of_clubs.png')`,"weight":9},
+  {"dir":`url('9_of_diamonds.png')`,"weight":9},
+  {"dir":`url('9_of_hearts.png')`,"weight":9},
+  {"dir":`url('9_of_spades.png')`,"weight":9},
+
+  {"dir":`url('10_of_clubs.png')`,"weight":10},
+  {"dir":`url('10_of_diamonds.png')`,"weight":10},
+  {"dir":`url('10_of_hearts.png')`,"weight":10},
+  {"dir":`url('10_of_spades.png')`,"weight":10},
+
+  {"dir":`url('jack_of_clubs2.png')`,"weight":2},
+  {"dir":`url('jack_of_diamonds2.png')`,"weight":2},
+  {"dir":`url('jack_of_hearts2.png')`,"weight":2},
+  {"dir":`url('jack_of_spades2.png')`,"weight":2},
+
+  {"dir":`url('queen_of_clubs2.png')`,"weight":3},
+  {"dir":`url('queen_of_diamonds2.png')`,"weight":3},
+  {"dir":`url('queen_of_hearts2.png')`,"weight":3},
+  {"dir":`url('queen_of_spades2.png')`,"weight":3},
+
+  {"dir":`url('king_of_clubs2.png')`,"weight":4},
+  {"dir":`url('king_of_diamonds2.png')`,"weight":4},
+  {"dir":`url('king_of_hearts2.png')`,"weight":4},
+  {"dir":`url('king_of_spades2.png')`,"weight":4},
+
+  {"dir":`url('ace_of_clubs.png')`,"weight":11},
+  {"dir":`url('ace_of_diamonds.png')`,"weight":11},
+  {"dir":`url('ace_of_hearts.png')`,"weight":11},
+  {"dir":`url('ace_of_spades2.png')`,"weight":11},
+];
+
+document.getElementById("generator").addEventListener("click", genRandom);
+document.getElementById("restart").addEventListener("click",() => {
+  userScoreElement1.innerText=0;
+  userScoreElement2.innerText=0;
+  userNumElement1.style.removeProperty("background-image");
+  userNumElement2.style.removeProperty("background-image");
+  scoreTable.children[0].replaceChildren('');
+  scoreTable.children[1].replaceChildren('');
+  numOfGames=0;
+  document.getElementById("userScore1").classList.remove("winner");
+  document.getElementById("userScore2").classList.remove("winner");
+  document.getElementById("userScore1").classList.remove("loser");
+  document.getElementById("userScore2").classList.remove("loser");
+});
+
+function genRandom(){
+  var keepGenerating = true;
+  var rand1, rand2;
+  while(keepGenerating){
+  rand1 = parseInt(Math.random()*cards.length);
+  rand2 = parseInt(Math.random()*cards.length);
+  if (rand1 != rand2) keepGenerating=false;
+  }
+  
+  setNumbers(rand1,rand2);
+  incrementWinner(rand1,rand2);
+  writeResults(rand1,rand2);
+  
+
+}
+
+
+
+  function writeResults(Rand1,Rand2){
+  var interResults = document.createElement("tr");
+  
+  var p1Results = document.createElement("td");
+  p1Results.innerText= cards[Rand1].weight;
+  var p2Results = document.createElement("td");
+  p2Results.innerText= cards[Rand2].weight;
+  
+  var roundResults = document.createElement("td");
+  roundResults.innerText= (Rand1 > Rand2) ? document.getElementById("userName1").innerText : document.getElementById("userName2").innerText ;
+  
+  interResults.append(p1Results,p2Results,roundResults);
+scoreTable.children[0].innerHTML = `<th>${document.getElementById("userName1").innerText}</th><th>${document.getElementById("userName2").innerText}</th><th>Winner</th>`;
+scoreTable.children[1].appendChild(interResults);
+}
+
+var numOfGames=0;
+function incrementWinner(Rand1,Rand2){
+  numOfGames++;
+    userScoreElement1.innerText = parseInt(userScoreElement1.innerText) + cards[Rand1].weight;
+    
+    userScoreElement2.innerText = parseInt(userScoreElement1.innerText) + cards[Rand2].weight;
+    
+  if (numOfGames == 3 ){
+   if (parseInt(userScoreElement1.innerText)>parseInt(userScoreElement2.innerText)){
+        alert(`Player ${document.getElementById("userName1").innerText} won! Congratulations!`);
+        document.getElementById("userScore1").classList.toggle("winner");
+        document.getElementById("userScore2").classList.toggle("loser");
+   }
+   else{
+    alert(`Player ${document.getElementById("userName2").innerText} won! Congratulations!`);
+    document.getElementById("userScore2").classList.toggle("winner");
+    document.getElementById("userScore1").classList.toggle("loser");
+   }
+  }
+}
+
+function setNumbers(Rand1,Rand2){
+  userNumElement1.style.setProperty("background-image",cards[Rand1].dir);
+  userNumElement2.style.setProperty("background-image",cards[Rand2].dir);
+}
+
